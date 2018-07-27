d31ca4f4ebf23ab70c4cf066d46559a825208f3d
tyler <tptorres5@gmail.com>
2018-07-27 16:45:54 -0400

    jobs app up and running

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')     # django model field; 
+    summary = models.CharField(max_length=200)  # puts a cap on the amount of text below the image
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -31,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'postgres',
+        'PASSWORD':'tychris91',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -118,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+#? The name in media root does not have to be the same as the url
