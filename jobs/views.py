4eb28cea8ce3711fe88fe7ceba4ba0a903555642
tyler <tptorres5@gmail.com>
2018-07-31 20:46:56 -0400

    Added bootstrap and jobs are now showing up

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+def home(request):
+    jobs = Job.objects #? Review this concept in the show jobs module on udemy
+    return render(request,'jobs/home.html', {'jobs':jobs}) # grabs everything from the database
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',jobs.views.home, name='home'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #this is where you should look for the information
